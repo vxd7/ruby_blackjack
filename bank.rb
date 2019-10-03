7d8d870fc029f25c074314dd52e8dbcda385251f
vxd7 <ykonpaku13@gmail.com>
2019-10-03 13:50:50 +0300

    Added some getter functions to the Bank class

--- bank.rb
+++ bank.rb
@@ -6,15 +6,34 @@ class Bank
     raise ValueError, 'Invalid initial player bank' if sum <= 0
 
     @player_banks = {}
-    @sum_bets = 0
-    players.each { |player| @player_banks[player] = sum }
+    @player_bets = {}
+    players.each do |player|
+      @player_banks[player] = sum
+      @player_bets[player] = 0
+    end
   end
 
   def make_bet(player, bet)
     raise ValueError, 'No such player' unless @player_banks.include?(player)
 
     @player_banks[player] -= bet
-    @sum_bets += bet
+    @player_bets[player] += bet
+  end
+
+  def player_bank(player)
+    raise ValueError, 'No such player' unless @player_banks.include?(player)
+
+    @player_banks[player]
+  end
+
+  def player_bet(player)
+    raise ValueError, 'No such player' unless @player_banks.include?(player)
+
+    @player_bets[player]
+  end
+
+  def all_bets
+    @player_bets.values.sum
   end
 
   private
@@ -22,7 +41,7 @@ class Bank
   def reward_winner(player)
     raise ValueError, 'No such player' unless @player_banks.include?(player)
 
-    @player_banks[player] += @sum_bets
-    @sum_bets = 0
+    @player_banks[player] += all_bets
+    @player_bets.keys.each { |pl| @player_bets[pl] = 0 }
   end
 end
