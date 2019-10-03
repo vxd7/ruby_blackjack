222f9fd8dfc5963e63825d76f807ef33d7c39515
vxd7 <ykonpaku13@gmail.com>
2019-10-03 14:13:42 +0300

    Added ComputerPlayer class
    Derived from Player class

---
+++ computer_player.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class ComputerPlayer < Player
+  def initialize(bank)
+    super('ComputerPlayer', bank)
+  end
+
+  def calculate_move
+    if hand_value >= 17
+      move_skip
+    else
+      move_add_card
+    end
+  end
+end
