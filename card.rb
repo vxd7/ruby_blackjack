50da783a94031ebcc09d03751e3da732d9778abe
vxd7 <ykonpaku13@gmail.com>
2019-10-03 14:15:16 +0300

    Added points calculation and string conversion methods to Card

--- card.rb
+++ card.rb
@@ -1,14 +1,23 @@
 # frozen_string_literal: true
 
+require_relative 'card_misc'
+
 class Card
   include CardMisc
-  attr_reader :suit, :color, :points
+  attr_reader :suit, :color
 
   def initialize(suit, color)
     @suit = suit
     @color = color
-    @points = card_points(@suit, @color)
   end
 
-  def to_s; end
+  def points(hand_sum)
+    raise ValueError, 'Need to know the current card sum on hands' if hand_sum.nil?
+
+    card_points(@color, hand_sum)
+  end
+
+  def to_s
+    card_to_str(@suit, @color)
+  end
 end
