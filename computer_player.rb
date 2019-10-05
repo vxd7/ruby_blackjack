2e3e6b662e0ecf4e589e27908c7c53280215e43f
vxd7 <ykonpaku13@gmail.com>
2019-10-06 01:57:37 +0300

    Added constants

--- computer_player.rb
+++ computer_player.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 class ComputerPlayer < Player
+  DECISION_HAND_VALUE = 17
+  MAX_CARD_NUMBER = 3
   def initialize(bank)
     super('ComputerPlayer', bank)
   end
 
   def calculate_move
-    if hand_value >= 17 || number_cards == 3
+    if hand_value >= DECISION_HAND_VALUE || number_cards == MAX_CARD_NUMBER
       move_skip
     else
       move_add_card
