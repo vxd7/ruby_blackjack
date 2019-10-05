e8aec843e6102b90095a3474bc33107c5777a87a
vxd7 <ykonpaku13@gmail.com>
2019-10-06 02:16:26 +0300

    Moved hand related methods from Player to Hand

--- hand.rb
+++ hand.rb
@@ -10,4 +10,20 @@ class Hand
   def take_card(card)
     @cards << card
   end
+
+  def number_cards
+    @cards.length
+  end
+
+  def hand_value
+    overall_value = 0
+    has_ace = false
+    @cards.each do |card|
+      has_ace = true if card.color == 'ace'
+      overall_value += card.points(overall_value)
+    end
+
+    overall_value -= 10 if has_ace && overall_value > 21
+    overall_value
+  end
 end
--- player.rb
+++ player.rb
@@ -16,24 +16,8 @@ class Player
     (@hand << cards).flatten!
   end
 
-  def hand_value
-    overall_value = 0
-    has_ace = false
-    @hand.each do |card|
-      has_ace = true if card.color == 'ace'
-      overall_value += card.points(overall_value)
-    end
-
-    overall_value -= 10 if has_ace && overall_value > 21
-    overall_value
-  end
-
   def empty_hand
     @hand = []
-  end
-
-  def number_cards
-    @hand.length
   end
 
   def money_in_bank
